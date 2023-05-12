bb7494cd0f58cbff619d34b4f584179688b34565
yahor-mishuk <yahor.mishuk@digitalinspirationlabs.io>
2023-05-12 14:44:10 +0300

    linting

--- app/core/admin.py
+++ app/core/admin.py
@@ -25,8 +25,17 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide', ),
-            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser')
+            'fields': (
+                        'email',
+                        'password1',
+                        'password2',
+                        'name',
+                        'is_active',
+                        'is_staff',
+                        'is_superuser'
+                    )
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
