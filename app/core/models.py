77faf6fb7ab2779cff814b2a8f12d3fdd7b48931
yahor-mishuk <yahor.mishuk@digitalinspirationlabs.io>
2023-05-12 14:42:11 +0300

    admin

--- app/core/models.py
+++ app/core/models.py
@@ -16,6 +16,13 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
